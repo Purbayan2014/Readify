208286b3caa0f416c257ed0bfe553683aafc9e5f
purbayan2014 <purbayan2014@gmail.com>
2023-04-29 20:46:19 +0530

    added compoenent for searching for top books

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './components/Home/ExploreTopBooks';
 
 
 function App() {
   return (
+    <>
     <Navbar />
+    <ExploreTopBooks />
+    </>
   );
 }
 
