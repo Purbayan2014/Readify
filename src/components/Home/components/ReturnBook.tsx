7f874694c78627ae4ac6c385800988ec7396b3a0
purbayan2014 <purbayan2014@gmail.com>
2023-04-30 19:06:11 +0530

    oops missed this in the prev commit

---
+++ src/components/Home/components/ReturnBook.tsx
@@ -0,0 +1,18 @@
+import { ReturnBookProps } from '../../../types';
+
+export const ReturnBook = (props: ReturnBookProps ) => {
+    const { bookImageName, bookTitle } = props;
+    return (
+      <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+        <div className="text-center">
+        <img src={bookImageName} width="151" height="233" alt="book" />
+        <h6 className="mt-2">{bookTitle}</h6>
+          <p>Readify</p>
+          <a className="btn main-color text-white" href="#">
+            Reserve
+          </a>
+        </div>
+      </div>
+    );
+  };
+  
